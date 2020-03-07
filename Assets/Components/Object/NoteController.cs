b39b5fd4d1a3e826481ec6b8d65230ca6dba4660
Kantanat Siripipatworakun <phantomktn@gmail.com>
2020-03-07 23:11:07 +0700

    Add readable note Asset

---
+++ Assets/Components/Object/NoteController.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NoteController : MonoBehaviour
+{
+    public Image noteImage;
+    // Start is called before the first frame update
+    void Start()
+    {
+        noteImage.enabled = false;
+    }
+
+    // Update is called once per frame
+    public void showNote()
+    {
+        noteImage.enabled = true;
+    }
+    public void hideNote()
+    {
+        noteImage.enabled = false;
+    }
+}
