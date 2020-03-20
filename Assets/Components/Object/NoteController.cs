4a0c07849ca3103de4119bb383e7f01bf9e43161
Kantanat Siripipatworakun <phantomktn@gmail.com>
2020-03-20 13:41:35 +0700

    Edit Reading note and add to scene

--- Assets/Components/Object/NoteController.cs
+++ Assets/Components/Object/NoteController.cs
@@ -6,8 +6,9 @@ public class NoteController : MonoBehaviour
 {
   public ChickenController player;
   public GameObject note;
-  public float minDist = 5f;
-  public string text = "There's someting on high place\nPress 'Esc' to cancel.";
+  public float minDist = 2f;
+  public string text = "Press 'E' to read.";
+
   private float dist;
   private bool reading = false;
   // private float h = Screen.height*1f/2;
@@ -21,15 +22,18 @@ public class NoteController : MonoBehaviour
       if (Input.GetKey(KeyCode.E))
       {
         reading = true;
+        text = "There's someting on high place\n\nPress 'Esc' to cancel.";
       }
-      if (reading && Input.GetKey(KeyCode.Escape))
+      else if (reading && Input.GetKey(KeyCode.Escape))
       {
         reading = false;
+        text = "Press 'E' to read.";
       }
     }
     else
     {
       reading = false;
+      text = "Press 'E' to read.";
     }
   }
 
@@ -37,11 +41,7 @@ public class NoteController : MonoBehaviour
   {
     if (dist <= minDist)
     {
-      GUI.TextArea(new Rect(100, 50, 200, 100), "Press 'E' to read.");
-      if (reading)
-    {
-      GUI.TextArea(new Rect(100, 50, 200, 100), text);
-    }
+      GUI.TextArea(new Rect(100, 50, 200, 100), text);      
     }
   }
 }
