4658c55043cce917ab64e644845b198e795241a1
KNTXTremE <kan.horwang@gmail.com>
2020-03-22 22:49:58 +0700

    Move predator health bar logic to closet

--- Assets/Components/Object/KeyController.cs
+++ Assets/Components/Object/KeyController.cs
@@ -7,21 +7,20 @@ public class KeyController : MonoBehaviour
     public DoorController door;
     public bool active;
     public ClosetController closet;
-    public GameObject predatorHealthBar;
+
 
     // Start is called before the first frame update
     void Start()
     {
         this.active = true;
-        predatorHealthBar.SetActive(false);
     }
 
     private void OnCollisionEnter(Collision collision)
     {
-        if (this.active && collision.gameObject.tag == "Player") {
+        if (this.active && collision.gameObject.tag == "Player")
+        {
             if (closet) closet.Toggle();
             if (door) door.Activate();
-            if (predatorHealthBar) predatorHealthBar.SetActive(true);
             Destroy(gameObject);
         }
     }
