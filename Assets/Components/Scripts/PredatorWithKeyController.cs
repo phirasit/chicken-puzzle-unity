3297e2af41569fc7a9010462cca370a6304fb26e
KNTXTremE <kan.horwang@gmail.com>
2020-03-22 22:52:28 +0700

    Embbeded key into predator in scene 2

---
+++ Assets/Components/Scripts/PredatorWithKeyController.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PredatorWithKeyController : MonoBehaviour
+{
+    public GameObject key;
+    public PredatorController predator;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        key.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (predator.GetHP() == 0)
+        {
+            if (key)
+            {
+                key.transform.position = predator.transform.position;
+                key.SetActive(true);
+            }
+        }
+    }
+}
