4914ccefa1fc90fd6cc268830f9a5244d1668e19
KNTXTremE <kan.horwang@gmail.com>
2020-03-22 22:52:01 +0700

    Implemented breakable wall logic

--- Assets/Components/Object/WeakWallController.cs
+++ Assets/Components/Object/WeakWallController.cs
@@ -4,27 +4,41 @@ using UnityEngine;
 
 public class WeakWallController : MonoBehaviour
 {
-    // Start is called before the first frame update
     public ChickenController player;
-    public float WallHP = 5;
-    private float dist;
+    public ClosetController closet;
+    public float WallHP;
+    public float dist;
+
+    private float MaxWallHP = 15;
     private float minDist = 1.0f;
-    private string text = "This wall can be breakable.";
+    private string text = "Press G to destroy.";
+
+    private void Start()
+    {
+        WallHP = MaxWallHP;
+    }
 
     // Update is called once per frame
     void Update()
     {
         dist = Vector3.Distance(player.transform.position, gameObject.transform.position);
-        // if (hit on wall){
-        //  WallHP-= damage of chicken or something
-        // }
-        // if (wallHP <= 0) {
-            //break wall
-        // }
+        if (dist <= minDist && Input.GetKeyDown(KeyCode.G))
+        {
+            WallHP -= player.GetAttack();
+        }
+        if (WallHP <= 0)
+        {
+            if (closet) closet.Toggle();
+            gameObject.SetActive(false);
+        }
     }
-    //void OnGUI()
-    //{
-    //    if (dist <= minDist) return
-    //    GUI.TextArea(new Rect(100, 50, 200, 100), text);
-    //}
+
+    void OnGUI()
+    {
+        if (dist <= minDist)
+        {
+            GUI.TextArea(new Rect(100, 50, 200, 100), text);
+        }
+
+    }
 }
