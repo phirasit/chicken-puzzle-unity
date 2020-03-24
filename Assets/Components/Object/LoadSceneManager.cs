879d23c262017c3a849ae20f915e7f6dbd59a2b9
Kantanat Siripipatworakun <phantomktn@gmail.com>
2020-03-24 13:36:47 +0700

    add loadSceneManager

---
+++ Assets/Components/Object/LoadSceneManager.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+using System.Collections;
+public class LoadSceneManager : MonoBehaviour
+{
+    void OnTriggerEnter(Collider other)
+    {
+        SceneManager.LoadScene(1);
+    }
+}
